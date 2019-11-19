f7b24df5b0944d4e6f4ec9dab1bea0987088099a
shirleypaz <shirley.pazz@gmail.com>
2019-11-18 21:44:24 -0300

    ajuste lint

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-const app = require('./src/app')
-const port = 4000
+const app = require("./src/app");
+const port = 4000;
 
-app.listen(port, `Api rodando na porta ${port}`)
+app.listen(port, () => {
+  console.log(`Api rodando na porta ${port}`);
+});
